9ded38bde0b085ed54333170517562e157da133e
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-08-25 19:18:23 +0300

    3.14

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
@@ -10,81 +12,56 @@ app.use(morgan(':method :url :status :response-time ms - :res[content-length] :b
 app.use(cors())
 app.use(express.static('static_dist'))
 
-let data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    },
-    { 
-      "id": 5,
-      "name": "sq q", 
-      "number": "39-8-6423122"
-    }
-]
-
 app.post('/api/persons', (req, res) => {
     const content = req.body
     if (!content.name || !content.number) {
        return res.status(400).json({error: 'content missing'})
     }
-    if (data.find(per => per.name === content.name)) {
-        return res.status(400).json({error: 'name must be unique'})
-    }
-    const id = Math.floor(Math.random() * 1000000000000000)
-    console.log(req.body);
-    const newPerson = {"id": id, "name": content.name, "number": content.number }
-    data = data.concat(newPerson)
-    res.json(data)
+
+    const newPerson = new Person({
+        name: content.name,
+        number: content.number
+    })
+
+    newPerson.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
-app.get('/info', (req, res) => {
+const getCount = async () => {
+    return await Person.countDocuments({})
+}
+
+app.get('/info', async (req, res) => {
     res.send(
-        `<p>Phonebook has info for ${data.length} people</p>
+        `<p>Phonebook has info for ${await getCount()} people</p>
         <p>${new Date()}</p>
     `)
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(data)
+    Person.find({})
+    .then(persons => 
+        res.json(persons))
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = data.find(per => per.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    console.log("id", req.params.id);
+    Person.findById(req.params.id)
+    .then(per => {
+        res.json(per)})
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    data = data.filter(per => per.id !== id)
-    res.status(204).end()
+    Person.findByIdAndDelete(req.params.id).then(
+        res.status(204).end())
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+}) 
