47ccbc7ade99b1797bfb532a99326f9b90e3a5f8
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-08-25 19:50:49 +0300

    3.18

--- index.js
+++ index.js
@@ -1,67 +1,102 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const Person = require('./models/person')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const Person = require("./models/person");
+const app = express();
 
-morgan.token('body', (req, res) => JSON.stringify(req.body));
+morgan.token("body", (request, response) => JSON.stringify(request.body));
 
-app.use(express.json())
-app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
-app.use(cors())
-app.use(express.static('static_dist'))
+app.use(express.json());
+// app.use(
+//   morgan(
+//     ":method :url :status :response-time ms - :response[content-length] :body - :request[content-length]"
+//   )
+// );
+app.use(cors());
+app.use(express.static("static_dist"));
 
-app.post('/api/persons', (req, res) => {
-    const content = req.body
-    if (!content.name || !content.number) {
-       return res.status(400).json({error: 'content missing'})
-    }
+app.post("/api/persons", (request, response) => {
+  const content = request.body;
+  if (!content.name || !content.number) {
+    return response.status(400).json({ error: "content missing" });
+  }
 
-    const newPerson = new Person({
-        name: content.name,
-        number: content.number
-    })
+  const newPerson = new Person({
+    name: content.name,
+    number: content.number,
+  });
 
-    newPerson.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
-})
+  newPerson.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
+app.get("/", (request, response) => {
+  response.send("Hello world");
+});
 
 const getCount = async () => {
-    return await Person.countDocuments({})
-}
+  return await Person.countDocuments({});
+};
 
-app.get('/info', async (req, res) => {
-    res.send(
-        `<p>Phonebook has info for ${await getCount()} people</p>
+app.get("/info", async (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${await getCount()} people</p>
         <p>${new Date()}</p>
-    `)
+    `
+  );
+});
+
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => response.json(persons));
+});
+
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((per) => {
+      if (per) {
+        response.json(per);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id).then(response.status(204).end()).catch(error => next(error));
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons', (req, res) => {
-    Person.find({})
-    .then(persons => 
-        res.json(persons))
-})
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
 
-app.get('/api/persons/:id', (req, res) => {
-    console.log("id", req.params.id);
-    Person.findById(req.params.id)
-    .then(per => {
-        res.json(per)})
-})
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
 
-app.delete('/api/persons/:id', (req, res) => {
-    Person.findByIdAndDelete(req.params.id).then(
-        res.status(204).end())
-})
+  next(error);
+};
 
-const PORT = process.env.PORT || 3001
+// this has to be the last loaded middleware.
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-}) 
+  console.log(`Server running on port ${PORT}`);
+});
